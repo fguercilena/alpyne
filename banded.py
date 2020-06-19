d2c4a343cb96d9c2e597c77579f99f749fb59a30
Federico Maria Guercilena <fguercilena@theorie.ikp.physik.tu-darmstadt.de>
2020-06-19 15:53:26 +0200

    Some progress on the banded systems algorithm

--- banded.py
+++ banded.py
@@ -1,6 +1,20 @@
 import numpy as np
-from numba import njit, float64, boolean, void
-from scipy.linalg import lu
+from numba import njit
+
+
+def LU(A, p, q):
+
+    n = len(A)
+    LU = A.copy()
+
+    for k in range(n - 1):
+        for i in range(k + 1, min(k + p + 1, n)):
+            LU[i, k] /= LU[k, k]
+        for j in range(k + 1, min(k + q + 1, n)):
+            for i in range(k + 1, min(k + p + 1, n)):
+                LU[i, j] -= LU[i, k]*LU[k, j]
+
+    return LU
 
 
 def banded_LU(A, p, q):
@@ -10,44 +24,38 @@ def banded_LU(A, p, q):
 
     for k in range(n - 1):
         for i in range(k + 1, min(k + p + 1, n)):
-            LU[i, k] /= LU[k, k]
-        # for j in range(k + 1, min(k + q + 1, n)):
-            # for i in range(k + 1, min(k + p + 1, n)):
-                # LU[i, j] -= LU[i, k]*LU[k, j]
-
-    # return np.tril(LU, -1) + np.diag([1]*n), np.triu(LU)
-    return LU
-
-
-def true_banded_LU(A, p, q):
-
-    n = len(A)
-    LU = A.copy()
-
-    for k in range(n - 1):
-        for i in range(k + 1, min(k + p + 1, n)):
-            LU[i - k + q, k] /= LU[q, k]
-        # for j in range(k + 1, min(k + q + 1, n)):
-            # for i in range(k + 1, min(k + p + 1, n)):
-                # LU[i - j + q, j] -= LU[i - k + q, k]*LU[k - j + q, j]
+            LU[k - i + p - 1, k] /= LU[p - 1, k]
+        for j in range(k + 1, min(k + q + 1, n)):
+            for i in range(k + 1, min(k + p + 1, n)):
+                LU[j - i + p - 1, j] -= LU[k - i + p - 1, k]*LU[j - k + p - 1, j]
 
     return LU
 
 
-A = np.diag([5.]*5) + np.diag([1.]*4, 1) + np.diag([2.]*4, -1)
-B = np.array([[0, 2, 2, 2, 2.], [5, 5, 5, 5, 5], [1, 1, 1, 1, 0]])
+for n in range(10, 1000, 10):
 
-print(A, "\n")
-print(B, "\n")
+    a = np.random.random(n)*5
+    b = np.random.random(n)
+    c = np.random.random(n)
+    d = np.random.random(n)
+    e = np.random.random(n)
+    f = np.random.random(n)
+    z = np.zeros_like(a)
 
-LU = banded_LU(A, 1, 1)
-true_LU = true_banded_LU(B, 1, 1)
+    A = np.vstack((f, e, z, b, a, c, d))
+    oA = np.diag(a) + np.diag(b[1:], -1) + np.diag(c[:-1], 1) + np.diag(d[:-2], 2) + np.diag(e[3:], -3) + np.diag(e[4:], -4)
+
+    LUm = LU(oA, 4, 2)
+    L = np.tril(LUm)
+    for i in range(n):
+        L[i, i] = 1
+    U = np.triu(LUm)
+    print(np.max(np.abs(oA - L@U)))
+
+    LUm = LU(oA, 4, 2)
+    L = np.tril(LUm)
+    for i in range(n):
+        L[i, i] = 1
+    U = np.triu(LUm)
 
 
-print(np.diagonal(LU, -1), true_LU[0], "\n")
-print(np.diagonal(LU, 0), true_LU[1], "\n")
-print(np.diagonal(LU, 1), true_LU[2], "\n")
-
-print(lu(A)[0])
-print(lu(A)[1])
-print(lu(A)[2])
