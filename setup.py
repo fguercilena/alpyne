e2f9261b271e0529c1eb9f63b796c1a02e5b0fbb
Federico Maria Guercilena <fguercilena@theorie.ikp.physik.tu-darmstadt.de>
2020-08-10 11:37:02 +0200

    Some deletions, added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="alpyne",
+    version="0.1.0",
+    author="Federico Maria Guercilena",
+    author_email="fguercilena@theorie.ikp.physik.tu-darmstadt.de",
+    packages=["alpyne"],
+    include_package_data=True,
+    description="Alpyne - ALgorithms coded in PYthon, Numba Enhanced",
+    zip_safe=False
+)
